a6f7f6cb8375929c2f30839e5a1eff34d0fd6e72
Alhad Pofali <alhadpofali@AlhadEternumScm.local>
2018-05-25 22:36:56 +0530

    update layers for Cifar1018

--- Deep_Model_Class_Students-test1.py
+++ Deep_Model_Class_Students-test1.py
@@ -44,10 +44,11 @@ class DeepModel:
     def cnn(self):
     # create model
         model = Sequential()
-        model.add(Conv2D(20, (5, 5), input_shape=self.input_shape, activation='relu'))
+        model.add(Conv2D(40, (5, 5), input_shape=self.input_shape, activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Conv2D(10, (3, 3), activation='relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2))) 
+        model.add(Conv2D(15, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(5, (3, 3), activation='relu'))
         #model.add(Dropout(0.2))
         model.add(Flatten())
         model.add(Dense(128, activation='relu'))
